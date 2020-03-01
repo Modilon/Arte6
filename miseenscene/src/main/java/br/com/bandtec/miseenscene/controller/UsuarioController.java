32dbeb485ee84f70652b20accf174382c5c8efb7
Eduhkbr <edu.abreu09@hotmail.com>
2020-03-01 15:30:56 -0300

    Logoff concluido

--- miseenscene/src/main/java/br/com/bandtec/miseenscene/controller/UsuarioController.java
+++ miseenscene/src/main/java/br/com/bandtec/miseenscene/controller/UsuarioController.java
@@ -11,6 +11,8 @@ import java.util.List;
 @RequestMapping("/usuario")
 public class UsuarioController {
 
+    boolean logado = false;
+
     @Autowired
     private UsuarioService usuarioService;
 
@@ -25,6 +27,7 @@ public class UsuarioController {
         if(usuarioService.existeByLogin(login)){
             Usuario usuario = usuarioService.buscarPorLogin(login);
             if(usuario.getLogin().equals(login) && usuario.getSenha().equals(senha)){
+                logado = true;
                 return "Login realizado com sucesso, seja bem-vindo: " + login;
             }else{
                 return "Login ou senha invalidos";
@@ -34,13 +37,23 @@ public class UsuarioController {
         }
     }
 
+    @PostMapping("/logoff")
+    public String logoff(){
+        if(logado){
+            logado = false;
+            return "Logoff realizado com sucesso";
+        }else{
+            return "Erro ao realizar logoff";
+        }
+    }
+
     @PostMapping("/add/{email}/{login}/{senha}")
     public String salvarUsuario(@PathVariable("email") String email,
                                  @PathVariable("login") String login,
                                  @PathVariable("senha") String senha){
         Usuario usuario = new Usuario(email, login, senha);
         try{
-            if(usuarioService.existeByLogin(login)){
+            if(usuarioService.existeByLogin(usuario.getLogin())){
                 return "Login já cadastrado!";
             }else {
                 usuarioService.salvar(usuario);
